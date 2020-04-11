03ceab2a0e1dc4f6a6e070a3499a78f4f3b33f28
Jeff Carouth <jeff.carouth@ziffmedia.com>
2020-04-10 22:01:48 -0500

    Create version 1.0.0 of the Particle-Counter

---
+++ src/particle-counter.ino
@@ -0,0 +1,85 @@
+/*
+ * Project particle-counter
+ * Description:
+ * Author:
+ * Date:
+ */
+#include "Grove_ChainableLED.h"
+#include "Grove_4Digit_Display.h"
+
+int pinButton = D2;
+int pinLed = D7;
+
+ChainableLED leds(A4, A5, 1);
+TM1637 tm1637(D4, D5);
+
+int pushCount = 0;
+int currentState = 0;
+int lastState = 0;
+
+void setup()
+{
+  pinMode(pinButton, INPUT);
+  pinMode(pinLed, OUTPUT);
+
+  leds.init();
+  leds.setColorHSB(0, 0.0, 0.0, 0.0);
+
+  tm1637.init();
+  tm1637.set(BRIGHT_TYPICAL);
+  tm1637.point(POINT_OFF);
+  tm1637.display(0, 0);
+  tm1637.display(1, 0);
+  tm1637.display(2, 0);
+  tm1637.display(3, 0);
+}
+
+void loop()
+{
+  int buttonPressed = digitalRead(pinButton);
+
+  if(buttonPressed) 
+  {
+    currentState = 1;
+  } 
+  else 
+  {
+    currentState = 0;
+  }
+
+  if (currentState != lastState) {
+    if(buttonPressed)
+    {
+      double color = 0.0;
+      if (pushCount <= 200) {
+        color = 0.005 * pushCount;
+        pushCount++;
+      } else {
+        color = 0.0;
+        pushCount = 0;
+      }
+
+      digitalWrite(pinLed, HIGH);
+      leds.setColorHSB(0, color, 1.0, 0.5);
+
+      int countCopy = pushCount;
+
+      tm1637.display(3, countCopy % 10);
+      countCopy /= 10;
+      tm1637.display(2, countCopy % 10);
+      countCopy /= 10;
+      tm1637.display(1, countCopy % 10);
+      countCopy /= 10;
+      tm1637.display(0, countCopy % 10);
+    }
+    else
+    {
+      digitalWrite(pinLed, LOW);
+      leds.setColorHSB(0, 0.0, 0.0, 0.0);
+    }
+
+    lastState = currentState;
+  }
+  
+  delay(10);
+}
---
+++ target/1.5.0/photon/obj/src/user_module.o.d
@@ -0,0 +1,4 @@
+/Users/jcarouth/projects/personal/particle-counter/target/1.5.0/photon//obj/src/user_module.o: \
+ src/user_module.c ../../../dynalib/inc/dynalib.h
+
+../../../dynalib/inc/dynalib.h:
